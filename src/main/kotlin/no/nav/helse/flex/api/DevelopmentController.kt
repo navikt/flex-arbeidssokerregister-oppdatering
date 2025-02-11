7c92229cb2e9e337a5e484fb8499d3fae1e86feb
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2025-02-11 14:42:13 +0100

    Test sending av ArbeidssokerregisterStopp-meldinger

--- src/main/kotlin/no/nav/helse/flex/api/DevelopmentController.kt
+++ src/main/kotlin/no/nav/helse/flex/api/DevelopmentController.kt
@@ -1,15 +1,36 @@
 package no.nav.helse.flex.api
 
-import org.springframework.http.MediaType
+import no.nav.helse.flex.arbeidssoker.ArbeidssokerRegisterStopp
+import no.nav.helse.flex.arbeidssoker.ArbeidssokerRegisterStoppProducer
+import no.nav.helse.flex.logger
+import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
+import java.util.*
 
+@Profile("testdatareset")
 @Controller
-class DevelopmentController {
-    @GetMapping("/api/v1/development", produces = [MediaType.APPLICATION_JSON_VALUE])
+@RequestMapping("/api/v1/development")
+class DevelopmentController(
+    private val arbeidssokerRegisterStoppProducer: ArbeidssokerRegisterStoppProducer,
+) {
+    private val log = logger()
+
+    @GetMapping("/verifiser")
     @ResponseBody
-    fun developmentResponse(): DevelopmentResponse = DevelopmentResponse("Response.")
+    fun developmentResponse(): DevelopmentResponse = DevelopmentResponse("Verifisert.")
+
+    @PostMapping("/stopp")
+    @ResponseBody
+    fun sendArbeidssokerRegisterStopp(fnr: String) {
+        val id = UUID.randomUUID().toString()
+        val arbeidssokerRegisterStopp = ArbeidssokerRegisterStopp(id = id, fnr = fnr)
+        arbeidssokerRegisterStoppProducer.send(arbeidssokerRegisterStopp)
+        log.info("Sendt ArbeidssokerRegisterStopp med id: $id.")
+    }
 }
 
 data class DevelopmentResponse(
