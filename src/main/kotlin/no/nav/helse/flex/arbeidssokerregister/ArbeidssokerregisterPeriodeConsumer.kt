c51f8fb602c1e48d29454861cddc3070f70d1d2e
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2025-02-19 07:57:38 +0100

    Lytt på perioder fra Arbeidssøkerregisteret

---
+++ src/main/kotlin/no/nav/helse/flex/arbeidssokerregister/ArbeidssokerregisterPeriodeConsumer.kt
@@ -0,0 +1,31 @@
+package no.nav.helse.flex.arbeidssokerregister
+
+import no.nav.helse.flex.logger
+import no.nav.paw.arbeidssokerregisteret.api.v1.Periode
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.support.Acknowledgment
+import org.springframework.stereotype.Component
+
+@Component
+class ArbeidssokerregisterPeriodeConsumer {
+    private val log = logger()
+
+    @KafkaListener(
+        topics = [ARBEIDSSOKERREGISTER_PERIODE_TOPIC],
+        id = "flex-arbeidssokerregister-periode-v1",
+        containerFactory = "arbeidssokerregisterPeriodeListenerContainerFactory",
+        properties = ["auto.offset.reset = earliest"],
+    )
+    fun listen(
+        cr: ConsumerRecord<Long, Periode>,
+        acknowledgment: Acknowledgment,
+    ) {
+        cr.value().also {
+            log.info("Mottok periode med id: ${it.id}.")
+        }
+        acknowledgment.acknowledge()
+    }
+}
+
+const val ARBEIDSSOKERREGISTER_PERIODE_TOPIC = "paw.arbeidssokerperioder-v1"
