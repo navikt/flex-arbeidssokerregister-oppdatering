62bc7695c4cb53c967765719463e6f1fa398f28b
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2025-03-28 11:21:38 +0100

    Ikke prosesser slettede søknader i dry-run

--- src/main/kotlin/no/nav/helse/flex/sykepengesoknad/SykepengesoknadListener.kt
+++ src/main/kotlin/no/nav/helse/flex/sykepengesoknad/SykepengesoknadListener.kt
@@ -8,20 +8,23 @@ import no.nav.helse.flex.objectMapper
 import no.nav.helse.flex.sykepengesoknad.kafka.SykepengesoknadDTO
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.listener.ConsumerSeekAware
+import org.springframework.kafka.listener.ConsumerSeekAware.ConsumerSeekCallback
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
+import java.time.LocalDate
 
 @Component
 class SykepengesoknadListener(
     private val sykepengesoknadService: SykepengesoknadService,
     private val environmentToggles: EnvironmentToggles,
-) {
+) : ConsumerSeekAware {
     private val log = logger()
 
     @WithSpan
     @KafkaListener(
         topics = [SYKEPENGESOKNAD_TOPIC],
-        id = "flex-arbeidssokerregister-oppdatering-sykepengesoknad-v2",
+        id = "flex-arbeidssokerregister-oppdatering-sykepengesoknad-v4",
         containerFactory = "kafkaListenerContainerFactory",
         properties = ["auto.offset.reset = earliest"],
     )
@@ -30,18 +33,43 @@ class SykepengesoknadListener(
         acknowledgment: Acknowledgment,
     ) {
         cr.value().tilSykepengesoknadDTO().also {
+            if (
+                // Ikke prosesser søknader som er slettet, men som ligger på Kafka.
+                it.friskTilArbeidVedtakId in
+                listOf(
+                    "4fe42342-7102-44d8-acd9-dc6a4f226f5a",
+                    "e4504199-f052-469a-9e0d-bffd1bad6bef",
+                    "688142df-92d9-4f44-b176-fd74d0c5da1d",
+                )
+            ) {
+                return
+            }
+
             try {
                 sykepengesoknadService.behandleSoknad(it)
             } catch (e: Exception) {
-                if (!environmentToggles.erProduksjon()) {
-                    val clusterName = environmentToggles.naisClusterName()
-                    log.warn("Feil ved behandling av søknad: ${it.id} i $clusterName: ${e.message}", e)
-                } else {
+                log.error(
+                    "Feil ved behandling av søknad: ${it.id} med vedtaksperiodeId: ${it.friskTilArbeidVedtakId}.",
+                    e,
+                )
+                if (environmentToggles.erProduksjon()) {
                     throw e
                 }
             }
         }
         acknowledgment.acknowledge()
+    }
+
+    override fun onPartitionsAssigned(
+        assignments: Map<org.apache.kafka.common.TopicPartition?, Long?>,
+        callback: ConsumerSeekCallback,
+    ) {
+        // Startet lytting på iSyfo-topic 20. mars 00:00:00.
+        val startTimestamp = LocalDate.of(2025, 3, 20).toInstantAtStartOfDay().toEpochMilli()
+
+        assignments.keys.filterNotNull().forEach { topicPartition ->
+            callback.seekToTimestamp(topicPartition.topic(), topicPartition.partition(), startTimestamp)
+        }
     }
 }
 
