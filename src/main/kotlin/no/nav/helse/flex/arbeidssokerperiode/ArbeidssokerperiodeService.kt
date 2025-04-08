2ac2975d0006309b37874874261f9dd229c2ead8
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2025-04-08 08:50:41 +0200

    Slett kode som sendte StoppMelding for to vedtak

--- src/main/kotlin/no/nav/helse/flex/arbeidssokerperiode/ArbeidssokerperiodeService.kt
+++ src/main/kotlin/no/nav/helse/flex/arbeidssokerperiode/ArbeidssokerperiodeService.kt
@@ -1,52 +1,19 @@
 package no.nav.helse.flex.arbeidssokerperiode
 
-import no.nav.helse.flex.EnvironmentToggles
 import no.nav.helse.flex.logger
 import no.nav.helse.flex.sykepengesoknad.ArbeidssokerperiodeStoppProducer
 import no.nav.helse.flex.sykepengesoknad.StoppMelding
-import no.nav.helse.flex.sykepengesoknad.VedtaksperiodeExceptionRepository
 import no.nav.paw.arbeidssokerregisteret.api.v1.Periode
-import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.time.Instant
-import java.util.concurrent.TimeUnit
 
 @Service
 class ArbeidssokerperiodeService(
     private val arbeidssokerperiodeRepository: ArbeidssokerperiodeRepository,
     private val arbeidssokerperiodeStoppProducer: ArbeidssokerperiodeStoppProducer,
-    private val vedtaksperiodeExceptionRepository: VedtaksperiodeExceptionRepository,
-    private val environmentToggles: EnvironmentToggles,
 ) {
     private val log = logger()
-
-    @Scheduled(initialDelay = 2, fixedDelay = 3600, timeUnit = TimeUnit.MINUTES)
-    fun sendStoppMelding() {
-        if (environmentToggles.erProduksjon()) {
-            listOf(
-                "bedb05d3-a2ff-4ee3-8525-44965b21442c",
-                "5abde058-fc10-470f-a336-0daccd7ed733",
-            ).forEach {
-                vedtaksperiodeExceptionRepository.findByVedtaksperiodeId(it).first().also {
-                    arbeidssokerperiodeStoppProducer.send(
-                        StoppMelding(
-                            vedtaksperiodeId = it.vedtaksperiodeId,
-                            fnr = it.fnr,
-                            avsluttetTidspunkt = Instant.now(),
-                        ),
-                    )
-                    vedtaksperiodeExceptionRepository.deleteByFnrAndVedtaksperiodeId(
-                        fnr = it.fnr,
-                        vedtaksperiodeId = it.vedtaksperiodeId,
-                    )
-                    log.info(
-                        "Sendt StoppMelding for vedtaksperiode: ${it.vedtaksperiodeId} og slettet fra vedtaksperiode_exception.",
-                    )
-                }
-            }
-        }
-    }
 
     @Transactional
     fun behandlePeriode(periode: Periode) {
