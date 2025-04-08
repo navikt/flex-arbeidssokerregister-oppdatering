878f28d350348eae69234056c2f7a8155dbd3bef
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2025-04-08 12:50:05 +0200

    Konsumer paw.arbeidssokerperioder-v1 med flere tråder

--- src/main/kotlin/no/nav/helse/flex/arbeidssokerregister/ArbeidssokerperiodeListener.kt
+++ src/main/kotlin/no/nav/helse/flex/arbeidssokerregister/ArbeidssokerperiodeListener.kt
@@ -2,26 +2,26 @@ package no.nav.helse.flex.arbeidssokerregister
 
 import io.opentelemetry.instrumentation.annotations.WithSpan
 import no.nav.helse.flex.arbeidssokerperiode.ArbeidssokerperiodeService
-import no.nav.helse.flex.sykepengesoknad.toInstantAtStartOfDay
+import no.nav.helse.flex.logger
 import no.nav.paw.arbeidssokerregisteret.api.v1.Periode
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.springframework.kafka.annotation.KafkaListener
-import org.springframework.kafka.listener.ConsumerSeekAware
-import org.springframework.kafka.listener.ConsumerSeekAware.ConsumerSeekCallback
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
-import java.time.LocalDate
 
 @Component
 class ArbeidssokerperiodeListener(
     private val arbeidssokerperiodeService: ArbeidssokerperiodeService,
-) : ConsumerSeekAware {
+) {
+    private val log = logger()
+
     @WithSpan
     @KafkaListener(
         topics = [ARBEIDSSOKERPERIODE_TOPIC],
-        id = "flex-arbeidssokerregister-oppdatering-periode-v2",
+        id = "flex-arbeidssokerregister-oppdatering-periode-v",
         containerFactory = "avroKafkaListenerContainerFactory",
         properties = ["auto.offset.reset=earliest"],
+        concurrency = "3",
     )
     fun listen(
         cr: ConsumerRecord<Long, Periode>,
@@ -29,19 +29,11 @@ class ArbeidssokerperiodeListener(
     ) {
         cr.value().also {
             arbeidssokerperiodeService.behandlePeriode(it)
+            if (it.id.toString() == "7249ce01-ad44-4ddb-9285-dde2b74bec94") {
+                log.info("Fant arbeidssokerperiode med id: ${it.id} på partition: ${cr.partition()} og offset: ${cr.offset()}.")
+            }
         }
         acknowledgment.acknowledge()
-    }
-
-    override fun onPartitionsAssigned(
-        assignments: Map<org.apache.kafka.common.TopicPartition?, Long?>,
-        callback: ConsumerSeekCallback,
-    ) {
-        val startTimestamp = LocalDate.of(2025, 3, 20).toInstantAtStartOfDay().toEpochMilli()
-
-        assignments.keys.filterNotNull().forEach { topicPartition ->
-            callback.seekToTimestamp(topicPartition.topic(), topicPartition.partition(), startTimestamp)
-        }
     }
 }
 
